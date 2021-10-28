edb556ea48767d8bd78efdfb53b44d7ae8b91cc9
IssaDia <issadiapro@gmail.com>
2021-10-28 10:31:25 +0200

    first printout ok

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,20 @@
 class Calculator {
-    int a;
-    int b;
-    int result;
-    public static void main (int a, int b) {
+        public static void main (String[] args) {
+            int a;
+            int b;
+        int result;
+
         java.util.Scanner entree =   new java.util.Scanner(System.in);
             System.out.println("Donnez votre premier chiffre");
             a = entree.nextInt();
-            System.out.println("Donnez votre premier chiffre");
+            System.out.println("Donnez votre second chiffre");
+            b = entree.nextInt();
+            System.out.println("Donnez votre méthode de calcul");
+
 
 
 
 
     }
 
-    public static int add (int a, int b) {
-
-    }
 }
