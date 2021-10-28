83273a11b8e2ef14fb6cca66694b888712f63ec9
IssaDia <issadiapro@gmail.com>
2021-10-28 10:51:09 +0200

    Basic calculator's working

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,49 @@
+import java.util.Scanner;
+
 class Calculator {
         public static void main (String[] args) {
-            int a;
-            int b;
-        int result;
+            double a;
+            double b;
+            double result;
 
-        java.util.Scanner entree =   new java.util.Scanner(System.in);
-            System.out.println("Donnez votre premier chiffre");
-            a = entree.nextInt();
-            System.out.println("Donnez votre second chiffre");
-            b = entree.nextInt();
-            System.out.println("Donnez votre méthode de calcul");
+       Scanner scannerObject = new Scanner(System.in);
+do {
 
+System.out.print("Votre premier chiffre:\n");
+a = scannerObject.nextDouble();
+System.out.print("Votre second chiffre:\n");
+b = scannerObject.nextDouble();
 
+System.out.print("Choisissez votre opérateur dans cette liste (+, -, *, /, %, ^) :\n ");
+char operator = scannerObject.next().charAt(0);
+switch (operator) {
+case '+':
+result = a + b;
+break;
+case '-':
+result = a - b;
+break;
+case '*':
+result = a * b;
+break;
+case '/':
+result = a / b;
+break;
+case '%':
+result = a % b;
+break;
+case '^':
+result = Math.pow(a, b);
+break;
+case 'Q':
+System.exit(0);
+default:
+System.out.printf("Merci de spécifier un opérateur dans la liste");
+return;
+}            
 
-
-
-    }
-
+System.out.println("Votre résultat est celui ci " + result);
+} while (result != 'Q');
+scannerObject.close();
 }
+        }
