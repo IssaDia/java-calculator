4e49dad484eb44c22650d6365f5bc657c10ac236
IssaDia <issadiapro@gmail.com>
2021-10-28 10:20:33 +0200

    first commit

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+class Calculator {
+    int a;
+    int b;
+    int result;
+    public static void main (int a, int b) {
+        java.util.Scanner entree =   new java.util.Scanner(System.in);
+            System.out.println("Donnez votre premier chiffre");
+            a = entree.nextInt();
+            System.out.println("Donnez votre premier chiffre");
+
+
+
+
+    }
+
+    public static int add (int a, int b) {
+
+    }
+}
