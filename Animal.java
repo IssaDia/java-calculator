2dc42c12a6ac79b991c73647041512817284ca30
IssaDia <issadiapro@gmail.com>
2021-10-28 16:21:06 +0200

    Animal and Hippo classes added

---
+++ Animal.java
@@ -0,0 +1,35 @@
+public class Animal {
+    protected int weight;
+    protected int height;
+    protected int name;
+
+    public Animal(int weight, int height, int name) {
+        this.weight = weight;
+        this.height = height;
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+}
